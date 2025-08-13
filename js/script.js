7aa349728323ec4bef0c45cb1e460b59fed8dfb6
fapollzZz <kmilvat@mail.ru>
2025-08-13 20:37:45 +0700

    number2

--- js/script.js
+++ js/script.js
@@ -1,4 +1,21 @@
 "use strict";
 
-let numb = 5;
-const leftBorderWidth = 1;
+const number0Films = +prompt('Сколько вы уже посмотрели', '');
+
+const personalMovieDB = {
+    coutn: number0Films,
+    moveis: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+const a = prompt('Один из последних просмотренных фильмов',''),
+      b = prompt('На сколько оцените его?',''),
+      c = prompt('Один из последних просмотренных фильмов?',''),
+      d = prompt('На сколько оцените его?','');
+
+personalMovieDB.moveis[a] = b;
+personalMovieDB.moveis[c] = d;
+
+console.log(personalMovieDB);
